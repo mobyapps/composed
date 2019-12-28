e476d312fca6b8a961f3c1613fd7eb7d31deca1a
charescape <charescape@outlook.com>
2019-12-28 11:45:01 +0800

    Update startserv.sh

--- startserv.sh
+++ startserv.sh
@@ -18,17 +18,3 @@ chown -R user7:group7    /usr/local/nginx
 /usr/local/nginx/sbin/nginx
 
 chown -R user7:group7    /usr/local/nginx
-
-
-# for memcached
-chown -R user7:group7    /usr/local/memcached
-
-/usr/local/memcached/bin/memcached --user=root \
---port=11211 \
---memory-limit=128 \
---conn-limit=1024 \
---threads=4 \
---pidfile=/tmp/memcached.pid \
---daemon
-
-chown -R user7:group7    /usr/local/memcached
