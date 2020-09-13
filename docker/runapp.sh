00c51c061bdcac27099a353b0020ce956913503a
charescape <charescape@outlook.com>
2020-09-13 18:17:39 +0800

    Update runapp.sh

--- docker/runapp.sh
+++ docker/runapp.sh
@@ -2,6 +2,11 @@
 
 set -eux
 
+chown -R www-data:www-data /var/www
+chown -R www-data:www-data /usr/local/mysql
+chown -R www-data:www-data /usr/local/php
+chown -R www-data:www-data /usr/local/nginx
+
 /usr/local/mysql/support-files/mysql.server status
 /usr/local/mysql/support-files/mysql.server start
 
