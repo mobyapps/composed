f77b5a0fefc23fe339318965e4255d7d69c1bee5
charescape <charescape@outlook.com>
2020-09-13 16:06:26 +0800

    mysql

--- docker/runapp.sh
+++ docker/runapp.sh
@@ -2,8 +2,10 @@
 
 set -eux
 
+/usr/local/mysql/support-files/mysql.server start
+
 /usr/local/php/bin/php -v
-/usr/local/php/sbin/php-fpm --allow-to-run-as-root
+/usr/local/php/sbin/php-fpm
 
 /usr/local/nginx/sbin/nginx -t
 /usr/local/nginx/sbin/nginx
