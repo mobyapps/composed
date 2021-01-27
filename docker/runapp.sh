dcd2272a967e5be81349f5371405536f66791335
charescape <charescape@outlook.com>
2021-01-27 21:14:03 +0800

    Update runapp.sh

--- docker/runapp.sh
+++ docker/runapp.sh
@@ -5,12 +5,12 @@ set -eux
 chown -R www-data:www-data /var/www
 chown -R www-data:www-data /usr/local/mysql
 chown -R www-data:www-data /usr/local/php
-chown -R www-data:www-data /usr/local/nginx
+chown -R www-data:www-data /usr/local/openresty
 
 /usr/local/mysql/support-files/mysql.server start
 
 /usr/local/php/bin/php -v
 /usr/local/php/sbin/php-fpm
 
-/usr/local/nginx/sbin/nginx -t
-/usr/local/nginx/sbin/nginx
+/usr/local/openresty/nginx/sbin/nginx -t
+/usr/local/openresty/nginx/sbin/nginx
