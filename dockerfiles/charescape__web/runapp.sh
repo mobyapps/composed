f418d4b3fd3787ce6947495714ae0fa1d3c2b652
charescape <charescape@outlook.com>
2021-04-07 15:24:09 +0800

    Update runapp.sh

--- dockerfiles/charescape__web/runapp.sh
+++ dockerfiles/charescape__web/runapp.sh
@@ -5,10 +5,10 @@ set -eux
 
 # find /var/www -not \( -path "*/node_modules/*" -o -path "*/\.git/*" \) -exec chown www-data:www-data {} \;
 
-chown -R www-data:www-data /var/www
-chown -R www-data:www-data /usr/local/mysql
-chown -R www-data:www-data /usr/local/php
-chown -R www-data:www-data /usr/local/openresty
+chown -R www-data:www-data /var/www/
+chown -R www-data:www-data /usr/local/mysql/
+chown -R www-data:www-data /usr/local/php/
+chown -R www-data:www-data /usr/local/openresty/
 
 /usr/local/mysql/support-files/mysql.server start
 
