71225db5d3149426b1bcd6d35c55445c1440f9af
charescape <charescape@outlook.com>
2023-03-30 17:00:37 +0800

    release ubuntu22-php81.rv0

--- dockerfiles/charescape__web/lite/runapps.sh
+++ dockerfiles/charescape__web/lite/runapps.sh
@@ -6,11 +6,8 @@ set -eux
 # find /var/www -not \( -path "*/node_modules/*" -o -path "*/\.git/*" \) -exec chown www-data:www-data {} \;
 
 # chown -R www-data:www-data /var/www/
-chown -R www-data:www-data /usr/local/mysql/
 chown -R www-data:www-data /usr/local/php/
 chown -R www-data:www-data /usr/local/openresty/
-
-/usr/local/mysql/support-files/mysql.server start
 
 /usr/local/php/bin/php -v
 /usr/local/php/sbin/php-fpm
@@ -31,6 +28,5 @@ else
   /usr/local/openresty/nginx/sbin/nginx
 fi
 
-pgrep -l mysql
 pgrep -l php
 pgrep -l nginx
