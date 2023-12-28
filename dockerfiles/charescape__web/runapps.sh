8670d9cf92f2b81ecbc591e496f278c9298772ef
charescape <53265646+charescape@users.noreply.github.com>
2023-12-28 19:31:39 +0800

    Update runapps.sh

--- dockerfiles/charescape__web/runapps.sh
+++ dockerfiles/charescape__web/runapps.sh
@@ -3,9 +3,9 @@
 set -eux
 
 
-# find /var/www -not \( -path "*/node_modules/*" -o -path "*/\.git/*" \) -exec chown www-data:www-data {} \;
+# find /repos -not \( -path "*/node_modules/*" -o -path "*/\.git/*" \) -exec chown www-data:www-data {} \;
 
-# chown -R www-data:www-data /var/www/
+# chown -R www-data:www-data /repos/
 chown -R www-data:www-data /usr/local/mysql/
 chown -R www-data:www-data /usr/local/php/
 chown -R www-data:www-data /usr/local/openresty/
@@ -15,14 +15,14 @@ chown -R www-data:www-data /usr/local/openresty/
 /usr/local/php/bin/php -v
 /usr/local/php/sbin/php-fpm
 
-if [ -f /var/www/nginx-external.conf ]
+if [ -f /repos/nginx-external.conf ]
 then
-  /usr/local/openresty/nginx/sbin/nginx -c /var/www/nginx-external.conf -t
-  /usr/local/openresty/nginx/sbin/nginx -c /var/www/nginx-external.conf
+  /usr/local/openresty/nginx/sbin/nginx -c /repos/nginx-external.conf -t
+  /usr/local/openresty/nginx/sbin/nginx -c /repos/nginx-external.conf
 else
-  if [ -f /var/www/nginx-override.conf ]; then
-    rm -f                            /usr/local/openresty/nginx/conf/nginx.conf
-    cp /var/www/nginx-override.conf  /usr/local/openresty/nginx/conf/nginx.conf
+  if [ -f /repos/nginx-override.conf ]; then
+    rm -f                          /usr/local/openresty/nginx/conf/nginx.conf
+    cp /repos/nginx-override.conf  /usr/local/openresty/nginx/conf/nginx.conf
 
     chown -R www-data:www-data       /usr/local/openresty/
   fi
