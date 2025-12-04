be0d82e725f6a638371ed2dff50ca3507d6b7a65
charescape <53265646+charescape@users.noreply.github.com>
2025-12-04 17:54:30 +0800

    Update mariadb.sh

--- src/ubuntu24-php84-mysql84/mariadb.sh
+++ src/ubuntu24-php84-mysql84/mariadb.sh
@@ -22,8 +22,6 @@ libsnappy-dev \
 -DDEFAULT_CHARSET=utf8mb4 \
 -DDEFAULT_COLLATION=utf8mb4_0900_ai_ci \
 -DWITH_EXTRA_CHARSETS=all \
--DDOWNLOAD_BOOST=1 \
--DWITH_BOOST=/usr/local/boostcxx_build_1_87_0 \
 -DENABLED_LOCAL_INFILE=1 \
 -DMYSQL_TCP_PORT=3307 \
 -DMYSQL_UNIX_ADDR=/var/www/mariadb.sock \
